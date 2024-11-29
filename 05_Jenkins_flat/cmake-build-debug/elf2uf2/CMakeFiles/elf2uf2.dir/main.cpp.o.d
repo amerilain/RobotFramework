4e6270fc6f501bd82e2545110a9426ab1965e7e2
amerilain <amerilain@gmail.com>
2024-11-29 17:28:11 +0200

    Add 05_Jenkins without .git

---
+++ 05_Jenkins_flat/cmake-build-debug/elf2uf2/CMakeFiles/elf2uf2.dir/main.cpp.o.d
@@ -0,0 +1,69 @@
+CMakeFiles/elf2uf2.dir/main.cpp.o: \
+ /usr/share/pico_sdk/tools/elf2uf2/main.cpp /usr/include/stdc-predef.h \
+ /usr/include/c++/12.2.1/cstdio \
+ /usr/include/c++/12.2.1/aarch64-alpine-linux-musl/bits/c++config.h \
+ /usr/include/c++/12.2.1/aarch64-alpine-linux-musl/bits/os_defines.h \
+ /usr/include/c++/12.2.1/aarch64-alpine-linux-musl/bits/cpu_defines.h \
+ /usr/include/fortify/stdio.h /usr/include/stdio.h \
+ /usr/include/features.h /usr/include/bits/alltypes.h \
+ /usr/include/c++/12.2.1/map /usr/include/c++/12.2.1/bits/stl_tree.h \
+ /usr/include/c++/12.2.1/bits/stl_algobase.h \
+ /usr/include/c++/12.2.1/bits/functexcept.h \
+ /usr/include/c++/12.2.1/bits/exception_defines.h \
+ /usr/include/c++/12.2.1/bits/cpp_type_traits.h \
+ /usr/include/c++/12.2.1/ext/type_traits.h \
+ /usr/include/c++/12.2.1/ext/numeric_traits.h \
+ /usr/include/c++/12.2.1/bits/stl_pair.h \
+ /usr/include/c++/12.2.1/type_traits /usr/include/c++/12.2.1/bits/move.h \
+ /usr/include/c++/12.2.1/bits/utility.h \
+ /usr/include/c++/12.2.1/bits/stl_iterator_base_types.h \
+ /usr/include/c++/12.2.1/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/12.2.1/bits/concept_check.h \
+ /usr/include/c++/12.2.1/debug/assertions.h \
+ /usr/include/c++/12.2.1/bits/stl_iterator.h \
+ /usr/include/c++/12.2.1/bits/ptr_traits.h \
+ /usr/include/c++/12.2.1/debug/debug.h \
+ /usr/include/c++/12.2.1/bits/predefined_ops.h \
+ /usr/include/c++/12.2.1/bits/allocator.h \
+ /usr/include/c++/12.2.1/aarch64-alpine-linux-musl/bits/c++allocator.h \
+ /usr/include/c++/12.2.1/bits/new_allocator.h /usr/include/c++/12.2.1/new \
+ /usr/include/c++/12.2.1/bits/exception.h \
+ /usr/include/c++/12.2.1/bits/memoryfwd.h \
+ /usr/include/c++/12.2.1/bits/stl_function.h \
+ /usr/include/c++/12.2.1/backward/binders.h \
+ /usr/include/c++/12.2.1/ext/alloc_traits.h \
+ /usr/include/c++/12.2.1/bits/alloc_traits.h \
+ /usr/include/c++/12.2.1/bits/stl_construct.h \
+ /usr/include/c++/12.2.1/ext/aligned_buffer.h \
+ /usr/include/c++/12.2.1/bits/stl_map.h \
+ /usr/include/c++/12.2.1/initializer_list /usr/include/c++/12.2.1/tuple \
+ /usr/include/c++/12.2.1/bits/uses_allocator.h \
+ /usr/include/c++/12.2.1/bits/invoke.h \
+ /usr/include/c++/12.2.1/bits/stl_multimap.h \
+ /usr/include/c++/12.2.1/bits/range_access.h \
+ /usr/include/c++/12.2.1/bits/erase_if.h /usr/include/c++/12.2.1/set \
+ /usr/include/c++/12.2.1/bits/stl_set.h \
+ /usr/include/c++/12.2.1/bits/stl_multiset.h \
+ /usr/include/c++/12.2.1/vector \
+ /usr/include/c++/12.2.1/bits/stl_uninitialized.h \
+ /usr/include/c++/12.2.1/bits/stl_vector.h \
+ /usr/include/c++/12.2.1/bits/stl_bvector.h \
+ /usr/include/c++/12.2.1/bits/functional_hash.h \
+ /usr/include/c++/12.2.1/bits/hash_bytes.h \
+ /usr/include/c++/12.2.1/bits/refwrap.h \
+ /usr/include/c++/12.2.1/bits/vector.tcc /usr/include/c++/12.2.1/cstring \
+ /usr/include/fortify/string.h /usr/include/string.h \
+ /usr/include/fortify/strings.h /usr/include/strings.h \
+ /usr/include/c++/12.2.1/cstdarg /usr/include/stdarg.h \
+ /usr/include/c++/12.2.1/algorithm \
+ /usr/include/c++/12.2.1/bits/stl_algo.h \
+ /usr/include/c++/12.2.1/bits/algorithmfwd.h \
+ /usr/include/c++/12.2.1/bits/stl_heap.h \
+ /usr/include/c++/12.2.1/bits/stl_tempbuf.h \
+ /usr/include/c++/12.2.1/bits/uniform_int_dist.h \
+ /usr/include/c++/12.2.1/cstdlib /usr/include/fortify/stdlib.h \
+ /usr/include/stdlib.h /usr/include/alloca.h \
+ /usr/include/c++/12.2.1/bits/std_abs.h \
+ /usr/share/pico_sdk/src/common/boot_uf2/include/boot/uf2.h \
+ /usr/include/stdint.h /usr/include/bits/stdint.h /usr/include/assert.h \
+ /usr/share/pico_sdk/tools/elf2uf2/elf.h
